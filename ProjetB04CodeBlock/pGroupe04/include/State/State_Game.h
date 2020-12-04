ac5feae613707798a5d08132cece58205ac96c3c
Skias <loiccena@live.be>
2020-12-04 23:05:21 +0100

    Second Merge
    Fixed some defects during previous merge

--- ProjetB04CodeBlock/pGroupe04/include/State/State_Game.h
+++ ProjetB04CodeBlock/pGroupe04/include/State/State_Game.h
@@ -24,43 +24,6 @@ using namespace std;
 class State_Game : public BaseState
 {
 private:
-    sf::Font m_font;         //Police du texte affiche
-    sf::Text m_text;         //Texte que l'on va afficher
-    float m_timePassed;      //Temps passe sur le state
-    sf::Texture m_bgTexture; // Texture du background
-    sf::Sprite m_bgSprite;   // Sprite du background
-
-    sf::RectangleShape m_health;     //Rectangle defissant le total de vie de l'ennemie
-    sf::RectangleShape m_lostHealth; //Rectangle defissant la vie restante de l'ennemie
-    sf::CircleShape m_btnEndTurn;    //Bouton servant a mettre fin au tour du joueur
-
-    Fight m_fight;              //
-    std::vector<Card *> m_hand; // Main du joueur
-
-    void LoadHand();      // Permet de charger la main
-    void DisplayPlayer(); //Affiche le joueur
-    void DisplayHand();   //Affiche la main du joueur
-
-public:
-    State_Game(StateManager *l_stateManager); //Constructeur
-    virtual ~State_Game();                    //Destructeur
-
-    void OnCreate();  //Permet de creer les differents elements graphiques que l'on va retrouver dans la fenetre
-    void OnDestroy(); //Permet de supprimer les callbacks afin de liberer la memoire allouee a cet effet
-
-    void Activate();   //Methode qui d'activer certaines modifications si la state a deja ete utilisee
-    void Deactivate(); //Permet de supprimer les modifications si la state n'est plus utilisee
-
-    void Update(const sf::Time &l_time); //Met a jour ce qui est a l'ecran
-    void Draw();                         //Permet de dessiner les differents elements graphiques
-
-    void Option(EventDetails *l_details); //Permet de switcher vers le State option, et donc la "fenetre" d'option
-    void Pause(EventDetails *l_details);  //Permet de switcher vers le State Pause, et donc la "fenetre" de pause
-
-    void CardClick(EventDetails* l_details); //Permet de cliquer sur les cartes et effectuer l'action qui en decoule
-
-protected:
-private:
     sf::Font m_font; //Police du texte affiche
     sf::Text m_text; //Texte que l'on va afficher
     float m_timePassed; //Temps passe sur le state
@@ -81,7 +44,27 @@ private:
 
     int m_egg;
 
-    void MouseHover();                       //Permet de gerer les events lies au survols des elements par la souris
+
+    void CardClick(EventDetails* l_details); //Permet de cliquer sur les cartes et effectuer l'action qui en decoule
+    void MouseHover(); //Permet de gerer les events lies au survols des elements par la souris
+
+public:
+    State_Game(StateManager *l_stateManager); //Constructeur
+    virtual ~State_Game();                    //Destructeur
+
+    void OnCreate();  //Permet de creer les differents elements graphiques que l'on va retrouver dans la fenetre
+    void OnDestroy(); //Permet de supprimer les callbacks afin de liberer la memoire allouee a cet effet
+
+    void Activate();   //Methode qui d'activer certaines modifications si la state a deja ete utilisee
+    void Deactivate(); //Permet de supprimer les modifications si la state n'est plus utilisee
+
+    void Update(const sf::Time &l_time); //Met a jour ce qui est a l'ecran
+    void Draw();                         //Permet de dessiner les differents elements graphiques
+
+    void Option(EventDetails *l_details); //Permet de switcher vers le State option, et donc la "fenetre" d'option
+    void Pause(EventDetails *l_details);  //Permet de switcher vers le State Pause, et donc la "fenetre" de pause
+
+
 };
 
 #endif // STATE_GAME_H
